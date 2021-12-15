69e684d49d43105fe6ab9c8bb0cca9c1f8fbce80
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-12-14 19:20:07 -0600

    Se agregan archivos principles de create-react-app

---
+++ ulfix-frontend/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// Views
+import Home from './views/Home.js'
+import Profile from './views/Profile.js'
+import SignIn from './views/SignIn.js'
+import SignUp from './views/SignUp.js'
+import UserList from './views/UserList.js'
+
+function App (props) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [lenguage, setLenguage] = useState('es')
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />} />
+        <Route
+          path='/profile'
+          element={<Profile lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />}
+        />
+        <Route
+          path='/sign-in'
+          element={<SignIn lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />}
+        />
+        <Route
+          path='/sign-up'
+          element={<SignUp lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />}
+        />
+        <Route
+          path='/user-list'
+          element={<UserList lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ ulfix-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// Styles
+import './assets/css/blk-design-system-react.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
