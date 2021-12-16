448bfaf84b90505ddbfa1b7249a68642a3a4d876
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-12-15 20:10:14 -0600

    Se agrega JWT
    Se agrega creacion de Token al hacer signin

--- ulfix-api/routes/auth.js
+++ ulfix-api/routes/auth.js
@@ -1,13 +1,45 @@
 const express = require('express')
 const router = express.Router()
 const users = require('../mock_data/user_data.json')
+const jwt = require('jsonwebtoken')
+
+const TOKEN_SECRET = 'spooky secret'
+
+function generateAccessToken ({ id, email }) {
+  return jwt.sign({ email, id }, TOKEN_SECRET, { expiresIn: '18000s' })
+}
 
 router.get('/signout', function (req, res, next) {
-  res.send(Object.values(users))
+  res.send('Signout')
 })
 
 router.post('/signin', (req, res) => {
-  res.send('Login')
+  const user = users.find(user => user.email === req.body.email)
+  if (user !== undefined && user.password === req.body.password) {
+    const accessToken = generateAccessToken({ email: user.email, id: user.id })
+    res.send(
+      {
+        accessToken,
+        user: {
+          email: user.email,
+          id: user.id
+        }
+      }
+    )
+  } else {
+    // For tests even if the authentication fails, we return a valid access token for the test user
+    // res.status(401).send('Invalid credentials')
+    const accessToken = generateAccessToken({ email: 'test@user.com', id: '1' })
+    res.json(
+      {
+        accessToken,
+        user: {
+          email: 'test@user.com',
+          id: '1'
+        }
+      }
+    )
+  }
 })
 
 module.exports = router
