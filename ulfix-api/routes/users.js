54da587fc43ca645a57e535265331f952f586cbf
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-12-15 18:11:55 -0600

    Correccion de errores en ruta usuarios

--- ulfix-api/routes/users.js
+++ ulfix-api/routes/users.js
@@ -21,19 +21,18 @@ router.get('/:userId', function (req, res, next) {
 })
 
 router.post('/', (req, res) => {
-  const lastUserId = users[users.length - 1].id
+  const user = users.find(user => user.email === req.body.email)
+  const newUserId = parseInt(users[users.length - 1].id) + 1
 
   // Verify if user exists
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].email === req.body.email) {
-      res.status(400).send('User already exists')
-      return // stop the function
-    }
+  if (user !== undefined) {
+    res.status(404).send('User already exists')
+    return
   }
 
   // Create new user
   const newUser = {
-    id: (lastUserId + 1).toString(),
+    id: newUserId.toString(),
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
